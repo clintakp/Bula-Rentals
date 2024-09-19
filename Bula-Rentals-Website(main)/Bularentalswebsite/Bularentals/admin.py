387e65bf3a7617aa7f22a1bd200868b1f82f3773
clintakp <141821364+clintakp@users.noreply.github.com>
2024-09-20 11:11:39 +1200

    Update admin.py

--- Bula-Rentals-Website(main)/Bularentalswebsite/Bularentals/admin.py
+++ Bula-Rentals-Website(main)/Bularentalswebsite/Bularentals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vehicles
 
-# Register your models here.
+@admin.register(Vehicles)
+class VehiclesAdmin(admin.ModelAdmin):
+    list_display = ('license_plate', 'status', 'daily_rate', 'brand', 'model')
+    search_fields = ('license_plate', 'brand', 'model')
+  
