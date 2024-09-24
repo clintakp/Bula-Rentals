0e0a3d94ac60f75cce3035ab052f591c3e454908
clintakp <141821364+clintakp@users.noreply.github.com>
2024-09-24 15:58:37 +1200

    Update urls.py

--- Bula-Rentals-Website(main)/Bularentalswebsite/Bularentals/urls.py
+++ Bula-Rentals-Website(main)/Bularentalswebsite/Bularentals/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import contact_success, contact_us
+from .views import contact_success
 
 urlpatterns = [
     path('',views.home, name="home"), 
@@ -8,8 +8,7 @@ urlpatterns = [
     path('about_us', views.about_us, name="about_us"),
     path('fleet', views.fleet, name="fleet"),
     path('My_booking', views.My_booking, name="My_booking"),
-    path('contact/', contact_us, name='contact_us'),
     path('contact_us', views.contact_us, name="contact_us"),
-    path('contact-success/', contact_success, name='contact_success'),
+    path('contact-success', contact_success, name='contact_success'),
     path('vehicle_search', views.vehicle_search, name='vehicle_search') 
 ]
