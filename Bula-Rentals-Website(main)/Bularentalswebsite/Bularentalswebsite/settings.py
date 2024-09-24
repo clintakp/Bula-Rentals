134c7d94b51994be08e8bfa7b2cb0044635ce062
clintakp <141821364+clintakp@users.noreply.github.com>
2024-09-24 16:05:30 +1200

    Update settings.py

--- Bula-Rentals-Website(main)/Bularentalswebsite/Bularentalswebsite/settings.py
+++ Bula-Rentals-Website(main)/Bularentalswebsite/Bularentalswebsite/settings.py
@@ -132,12 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Bularentalswebsite/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#Email Config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'iankumar52@gmail.com'
-EMAIL_HOST_PASSWORD = 'Beychella25_'
 
 
